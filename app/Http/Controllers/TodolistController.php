39f193ce3613ef26174ae170840d638381f90e3d
sajib47 <shajibhossain19981998@gmail.com>
2023-02-26 06:06:48 -0600

    todolistshajib

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todolist = Todolist::all();
+        return view ('home',compact('todolist'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'content' => 'required'
+        ]);
+        Todolist::create($data);
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todolist $todolist)
+    {
+        $todolist->delete();
+        return back();
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <header class="site name">
+        <h1>DevenLight Todolist</h1>
+    </header>
+    
+    <div class="box">
+        <div class="container">
+            <div class = "content">
+                <h2>Type Here</h2>
+                <div>
+                    <input placeholder="text now here" type="text">
+                    <button>submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+</body>
+   
+
+</html>
